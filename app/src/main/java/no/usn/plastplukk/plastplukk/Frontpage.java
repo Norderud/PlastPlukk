472f0f429ef12425249ff24181933bb7dea1405c
Sannsynlighet <asmundno@live.no>
2019-02-20 14:46:29 +0100

    Opprettet Frontpage aktivitet

--- app/src/main/java/no/usn/plastplukk/plastplukk/Frontpage.java
+++ app/src/main/java/no/usn/plastplukk/plastplukk/Frontpage.java
@@ -1,13 +1,21 @@
 package no.usn.plastplukk.plastplukk;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class Frontpage extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_frontpage);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void registrerPlast(View view){
+        Intent messageIntent = new Intent(this, StartReg.class);
+        startActivity(messageIntent);
     }
 }
