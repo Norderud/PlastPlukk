ed7a9dcea6fe567494cefc3c99e7f16d24c1e7f5
Sannsynlighet <asmundno@live.no>
2019-02-20 16:44:54 +0100

    menu

--- app/src/main/java/no/usn/plastplukk/plastplukk/Frontpage.java
+++ app/src/main/java/no/usn/plastplukk/plastplukk/Frontpage.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 
 public class Frontpage extends AppCompatActivity {
@@ -13,7 +15,12 @@ public class Frontpage extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_frontpage);
     }
-
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
     public void registrerPlast(View view){
         Intent messageIntent = new Intent(this, StartReg.class);
         startActivity(messageIntent);
