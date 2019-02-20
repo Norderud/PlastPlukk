3831e8282174716d64d671ebf8bb2046cfa6c019
dnelvik <dnelvik@gmail.com>
2019-02-20 23:26:05 +0100

    Buttons, icons, hdpi, xdpi, xxdpi
    - Laget første utkast for starten av registreringens GUI
    - Opprettet ulike dimens.xml filer for hver skjermstørrelse

--- app/src/main/java/no/usn/plastplukk/plastplukk/StartReg.java
+++ app/src/main/java/no/usn/plastplukk/plastplukk/StartReg.java
@@ -2,6 +2,8 @@ package no.usn.plastplukk.plastplukk;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class StartReg extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class StartReg extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_reg);
+    }
+
+    public void test(View view){
+        Log.e("Test", "Test");
     }
 }
