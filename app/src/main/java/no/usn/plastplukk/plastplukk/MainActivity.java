0d1769e06f5afaad8231e96059a08abf8a874cb3
Åsmund Norderud <asmundno@live.no>
2019-02-25 16:10:34 +0100

    Forside

--- app/src/main/java/no/usn/plastplukk/plastplukk/MainActivity.java
+++ app/src/main/java/no/usn/plastplukk/plastplukk/MainActivity.java
@@ -3,23 +3,16 @@ package no.usn.plastplukk.plastplukk;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.View;
 
-public class Frontpage extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_frontpage);
-    }
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.menu, menu);
-        return true;
+        setContentView(R.layout.activity_main);
     }
     public void registrerPlast(View view){
         Intent messageIntent = new Intent(this, StartReg.class);
