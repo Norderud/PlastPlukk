5c3e2c8f87b890b5d52abd0a353e3c6c7409af6c
Ola <olastaalberg@gmail.com>
2019-02-26 14:36:48 +0100

    Lagt til behandling av bildet.

--- app/src/main/java/no/usn/plastplukk/plastplukk/KameraAktivitet.java
+++ app/src/main/java/no/usn/plastplukk/plastplukk/KameraAktivitet.java
@@ -1,60 +1,107 @@
 package no.usn.plastplukk.plastplukk;
 
 import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.ImageView;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import static android.os.Environment.getExternalStoragePublicDirectory;
 
 public class KameraAktivitet extends AppCompatActivity {
     final static int REQUEST_IMAGE_CAPTURE = 1;
     ImageView imageView;
-    Uri bildetsURI;
+    static String currentPhotoPath;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_kamera_aktivitet);
-        Button kameraKnapp =(Button)findViewById(R.id.kameraKnapp);
+        Button kameraKnapp = (Button) findViewById(R.id.kameraKnapp);
         imageView = findViewById(R.id.photoDisplay);
-        kameraKnapp.setOnClickListener(new OnClickListener() {
+        kameraKnapp.setOnClickListener(new View.OnClickListener() {
 
+            @Override
             public void onClick(View v) {
-                // TODO Auto-generated method stub
-                Intent takePictureIntent =new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-                    startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
-                }
-
+                dispatchTakePictureIntent();
             }
         });
     }
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                System.out.println(ex.getMessage() + "oggggg?? \n" + ex.toString());
+
+                // Error occurred while creating the File
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this,
+                        "com.example.android.fileprovider",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+            }
+        }
+    }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-            Bitmap photo = (Bitmap) data.getExtras().get("data");
-            imageView.setImageBitmap(photo);
+            Bundle extras = data.getExtras();
+            Bitmap photo = (Bitmap) extras.get("data");
 
-            //TODO Lagre bilde hvis bilde er ok
+            try {
+                createImageFile();
+            } catch (Exception e) {
+                //TODO Håndter this exception
+            }
 
-            // Gets real path of image so it can be uploaded
-            //bildetsURI = hentURI(getApplicationContext(), photo);
+            Bitmap image = BitmapFactory.decodeFile(currentPhotoPath);
+            imageView.setImageBitmap(image);
+
         }
         if (resultCode == Activity.RESULT_CANCELED) {
             //TODO hva skjer om man kansellerer
         }
     }
-    /*public Uri hentURI(Context inContext, Bitmap inImage) {
-        String path = MediaStore.Images.Media.insertImage(inContext.getContentResolver(), inImage, "Title", null);
-        return Uri.parse(path);
-    }*/
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = String.format("JPEG_%s_", timeStamp);
+        File storageDir = getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        currentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
 
 // Refferanse på bilde-koden er https://developer.android.com/training/camera/photobasics#java
 }
