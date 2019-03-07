109a297d04f4e862bbdfae2e22ff10eaacdaa277
Ola <olastaalberg@gmail.com>
2019-03-07 15:56:04 +0100

    Laget opplastingsprogram, men får det ikke til å fungere. Må teste på localhost først for å se om det er noe rettighetsgreier som er problem.

--- app/src/main/java/no/usn/plastplukk/plastplukk/KameraAktivitet.java
+++ app/src/main/java/no/usn/plastplukk/plastplukk/KameraAktivitet.java
@@ -1,41 +1,61 @@
 package no.usn.plastplukk.plastplukk;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.provider.OpenableColumns;
 import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Base64;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.HashMap;
 
 public class KameraAktivitet extends AppCompatActivity {
+    public static final String UPLOAD_URL = "http://itfag.usn.no/~102468/plast/upload.php";
+    public static final String UPLOAD_KEY = "image";
+
     final static int REQUEST_IMAGE_CAPTURE = 1;
     ImageView imageView;
     static String currentPhotoPath;
-    Button taBildeKnapp, videreFraKameraKnapp;
+    Button taBildeKnapp, lastOppKnapp;
+    String imageFileName = "";
+    private Bitmap bitMap;
+    private Uri photoURI;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_kamera_aktivitet);
         taBildeKnapp = (Button) findViewById(R.id.kameraKnapp);
-        videreFraKameraKnapp = (Button) findViewById(R.id.videreFraKamera);
+        lastOppKnapp = (Button) findViewById(R.id.videreFraKamera);
         imageView = findViewById(R.id.photoDisplay);
         taBildeKnapp.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
                 dispatchTakePictureIntent();
+            }
+        });
+        lastOppKnapp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                uploadImage();
             }
         });
     }
@@ -49,15 +69,14 @@ public class KameraAktivitet extends AppCompatActivity {
             try {
                 photoFile = createImageFile();
             } catch (IOException ex) {
-                System.out.println(ex.getMessage() + "oggggg?? \n");
                 ex.printStackTrace();
 
                 // Error occurred while creating the File
             }
             // Continue only if the File was successfully created
             if (photoFile != null) {
-                Uri photoURI = FileProvider.getUriForFile(this,
-                        "com.example.android.fileprovider",
+                photoURI = FileProvider.getUriForFile(this,
+                        "no.usn.plastplukk.plastplukk.fileprovider",
                         photoFile);
                 takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
                 startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
@@ -71,7 +90,7 @@ public class KameraAktivitet extends AppCompatActivity {
 
             loadImageFromFile();
             taBildeKnapp.setText("Ta nytt bilde?");
-            videreFraKameraKnapp.setVisibility(View.VISIBLE);
+            lastOppKnapp.setVisibility(View.VISIBLE);
         }
     }
     public void loadImageFromFile(){
@@ -80,7 +99,7 @@ public class KameraAktivitet extends AppCompatActivity {
         int targetW = imageView.getWidth();
         int targetH = imageView.getHeight();
 
-        // Get the dimensions of the bitmap
+        // Get the dimensions of the bitMap
         BitmapFactory.Options bmOptions = new BitmapFactory.Options();
         bmOptions.inJustDecodeBounds = true;
         BitmapFactory.decodeFile(currentPhotoPath, bmOptions);
@@ -94,20 +113,87 @@ public class KameraAktivitet extends AppCompatActivity {
         bmOptions.inJustDecodeBounds = false;
         bmOptions.inSampleSize = scaleFactor;
 
-        Bitmap bitmap = BitmapFactory.decodeFile(currentPhotoPath, bmOptions);
-        imageView.setImageBitmap(bitmap);
+        bitMap = BitmapFactory.decodeFile(currentPhotoPath, bmOptions);
+        imageView.setImageBitmap(bitMap);
     }
 
     private File createImageFile() throws IOException {
         // Create an image file name
-        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-        String imageFileName = String.format("JPEG_%s_.jpg", timeStamp);
-        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
-        File image = new File(storageDir, imageFileName);
-        // Save a file: path for use with ACTION_VIEW intents
-        currentPhotoPath = image.getAbsolutePath();
-        return image;
+        String state = Environment.getExternalStorageState();
+        if (Environment.MEDIA_MOUNTED.equals(state)) { // Only create file if external storage exists
+            String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+            imageFileName = String.format("JPEG_%s_.jpg", timeStamp);
+            File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+            File image = new File(storageDir, imageFileName);
+            // Save a file: path for use with ACTION_VIEW intents
+            currentPhotoPath = image.getAbsolutePath();
+            return image;
+        }
+        throw new IOException();
     }
 
 // Refferanse på bilde-koden er https://developer.android.com/training/camera/photobasics#java
+
+    //TODO: LAGE NOE SOM UPLOADER BILDET
+    private void uploadImage(){
+        class UploadImage extends AsyncTask<Bitmap,Void,String> {
+            ProgressDialog loading;
+            RequestHandler rh = new RequestHandler();
+
+            @Override
+            protected void onPreExecute() {
+                super.onPreExecute();
+            loading = ProgressDialog.show(KameraAktivitet.this, "Uploading Image", "Please wait...",true,true);
+        }
+
+            @Override
+            protected void onPostExecute(String s) {
+                super.onPostExecute(s);
+                loading.dismiss();
+                Toast.makeText(getApplicationContext(),s,Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            protected String doInBackground(Bitmap... params) {
+                Bitmap bitmap = params[0];
+                String uploadImage = getStringImage(bitmap);
+
+                HashMap<String,String> data = new HashMap<>();
+                data.put(UPLOAD_KEY, uploadImage);
+                data.put("name",getFileName(photoURI));
+                String result = rh.postRequest(UPLOAD_URL,data);
+                return result;
+            }
+        }
+        UploadImage ui = new UploadImage();
+        ui.execute(bitMap);
+    }
+    String getFileName(Uri uri){
+        String result = null;
+        if (uri.getScheme().equals("content")) {
+            Cursor cursor = getContentResolver().query(uri, null, null, null, null);
+            try {
+                if (cursor != null && cursor.moveToFirst()) {
+                    result = cursor.getString(cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME));
+                }
+            } finally {
+                cursor.close();
+            }
+        }
+        if (result == null) {
+            result = uri.getPath();
+            int cut = result.lastIndexOf('/');
+            if (cut != -1) {
+                result = result.substring(cut + 1);
+            }
+        }
+        return result;
+    }
+    public String getStringImage(Bitmap bmp){
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        bmp.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+        byte[] imageBytes = baos.toByteArray();
+        String encodedImage = Base64.encodeToString(imageBytes, Base64.DEFAULT);
+        return encodedImage;
+    }
 }
