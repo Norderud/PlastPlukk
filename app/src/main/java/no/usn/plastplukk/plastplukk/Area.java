53fdda46200b57a003367d47351cb51d3c52f59a
dnelvik <dnelvik@gmail.com>
2019-03-12 19:17:24 +0100

    Shared Preferances
    Fikset slik at verdiene lagres og hentes frem og tilbake mellom hver aktivitet.
    Fikset slik at valgene som ble tatt huskes.

--- app/src/main/java/no/usn/plastplukk/plastplukk/Area.java
+++ app/src/main/java/no/usn/plastplukk/plastplukk/Area.java
@@ -1,6 +1,8 @@
 package no.usn.plastplukk.plastplukk;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -12,25 +14,33 @@ public class Area extends AppCompatActivity {
 
     String kategori, underKategori, størrelse;
     boolean[] checkSvar;
+    CheckBox fjellCheck, skogCheck, elvCheck, kystCheck, innsjøCheck, byCheck;
     TextView feilMelding;
+    public static final String MY_PREFS_NAME = "MyPrefsFile";
+    SharedPreferences prefs;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_area);
 
-        Intent intent = getIntent();
-        kategori = intent.getStringExtra("Kategori");
-        underKategori = intent.getStringExtra("Underkategori");
-        størrelse = intent.getStringExtra("Størrelse");
-
+        prefs = getSharedPreferences(MY_PREFS_NAME, MODE_PRIVATE);
+        kategori =  prefs.getString("kategori", "Ingen");
+        underKategori = prefs.getString("Underkategori", "Ingen");
+        størrelse = prefs.getString("Størrelse", størrelse);
 
         feilMelding = findViewById(R.id.Feilmelding);
         checkSvar = new boolean[]{false, false, false, false, false, false};
     }
 
-    public void openKamera(View view){
+    @Override
+    protected void onResume() {
+        checkOnReturn();
+        super.onResume();
+    }
 
+    public void openKamera(View view){
         boolean check = false;
         for (int i=0; i<checkSvar.length; i++) {
             if (checkSvar[i])
@@ -43,27 +53,8 @@ public class Area extends AppCompatActivity {
         }
 
         Intent messageIntent = new Intent(this, KameraAktivitet.class);
-
-        if (checkSvar.length != 0)
-            messageIntent.putExtra("checkSvar", checkSvar);
-        messageIntent.putExtra("Kategori", kategori);
-        messageIntent.putExtra("Underkategori", underKategori);
-        if (størrelse!=null)
-            messageIntent.putExtra("Størrelse", størrelse);
-
-        Log.e("Kategori", kategori);
-        Log.e("Underkategori", underKategori);
-        if (størrelse != null)
-            Log.e("Størrelse", størrelse);
-
-        for (int i=0; i<checkSvar.length; i++)
-            Log.e("Område", ""+checkSvar[i]);
-
-
         startActivity(messageIntent);
     }
-
-
 
 
     public void checkBoxes(View view){
@@ -73,41 +64,114 @@ public class Area extends AppCompatActivity {
         switch(view.getId()) {
 
             case R.id.fjellCheck:
-                if (checked)
+                if (checked) {
                     checkSvar[0] = true;
-                else
+                    storeArray(checkSvar, "Checksvar", this);
+                }
+                else {
                     checkSvar[0] = false;
+                    storeArray(checkSvar, "Checksvar", this);
+                }
                 break;
             case R.id.skogCheck:
-                if (checked)
+                if (checked) {
                     checkSvar[1] = true;
-                else
+                    storeArray(checkSvar, "Checksvar", this);
+                }
+                else {
                     checkSvar[1] = false;
+                    storeArray(checkSvar, "Checksvar", this);
+                }
                 break;
             case R.id.elvCheck:
-                if (checked)
+                if (checked) {
                     checkSvar[2] = true;
-                else
+                    storeArray(checkSvar, "Checksvar", this);
+                }
+                else {
                     checkSvar[2] = false;
+                    storeArray(checkSvar, "Checksvar", this);
+                }
                 break;
             case R.id.kystCheck:
-                if (checked)
+                if (checked) {
                     checkSvar[3] = true;
-                else
+                    storeArray(checkSvar, "Checksvar", this);
+                }
+                else {
                     checkSvar[3] = false;
+                    storeArray(checkSvar, "Checksvar", this);
+                }
                 break;
             case R.id.innsjøCheck:
-                if (checked)
+                if (checked) {
                     checkSvar[4] = true;
-                else
+                    storeArray(checkSvar, "Checksvar", this);
+                }
+                else {
                     checkSvar[4] = false;
+                    storeArray(checkSvar, "Checksvar", this);
+                }
                 break;
             case R.id.byCheck:
-                if (checked)
+                if (checked) {
                     checkSvar[5] = true;
-                else
+                    storeArray(checkSvar, "Checksvar", this);
+                }
+                else {
                     checkSvar[5] = false;
+                    storeArray(checkSvar, "Checksvar", this);
+                }
                 break;
         }
+    }
+
+    public void checkOnReturn(){
+        boolean[] array = loadArray("Checksvar", this);
+
+        if (array.length == 0)
+            return;
+
+        checkSvar = array;
+
+        if (checkSvar[0]) {
+            fjellCheck = findViewById(R.id.fjellCheck);
+            fjellCheck.setChecked(true);
+        } if (checkSvar[1]) {
+            skogCheck = findViewById(R.id.skogCheck);
+            skogCheck.setChecked(true);
+        } if (checkSvar[2]) {
+            elvCheck = findViewById(R.id.elvCheck);
+            elvCheck.setChecked(true);
+        } if (checkSvar[3]) {
+            kystCheck = findViewById(R.id.kystCheck);
+            kystCheck.setChecked(true);
+        } if (checkSvar[4]) {
+            innsjøCheck = findViewById(R.id.innsjøCheck);
+            innsjøCheck.setChecked(true);
+        } if (checkSvar[5]) {
+            byCheck = findViewById(R.id.byCheck);
+            byCheck.setChecked(true);
+        }
+
+    }
+
+    public boolean storeArray(boolean[] array, String arrayName, Context mContext) {
+
+        SharedPreferences prefs = mContext.getSharedPreferences(MY_PREFS_NAME, 0);
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putInt(arrayName +"_size", array.length);
+
+        for(int i=0;i<array.length;i++)
+            editor.putBoolean(arrayName + "_" + i, array[i]);
+        return editor.commit();
+    }
+
+    public boolean[] loadArray(String arrayName, Context mContext) {
+        int size = prefs.getInt(arrayName + "_size", 0);
+        boolean array[] = new boolean[size];
+        for(int i=0;i<size;i++)
+        array[i] = prefs.getBoolean(arrayName + "_" + i, false);
+        return array;
     }
 }
