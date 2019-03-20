3abe1ed65a18ac21d304750ade20f1386151a5dd
Danay <dnelvik@gmail.com>
2019-03-20 13:08:26 +0100

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	.idea/caches/gradle_models.ser

--- app/src/main/java/no/usn/plastplukk/plastplukk/PlasticRegistering/ChooseCategoryActivity.java
+++ app/src/main/java/no/usn/plastplukk/plastplukk/PlasticRegistering/ChooseCategoryActivity.java
@@ -2,29 +2,53 @@ package no.usn.plastplukk.plastplukk.PlasticRegistering;
 
 import android.Manifest;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.net.ConnectivityManager;
 import android.os.Bundle;
 import android.provider.Settings;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
 import no.usn.plastplukk.plastplukk.R;
 
 public class ChooseCategoryActivity extends AppCompatActivity {
 
     public static final String MY_PREFS_NAME = "MyPrefsFile";
+    SharedPreferences.Editor editor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_reg);
-
+        SharedPreferences prefs = getSharedPreferences(MY_PREFS_NAME, MODE_PRIVATE);
+        editor = getSharedPreferences(MY_PREFS_NAME, MODE_PRIVATE).edit();
+        ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        if (connectivityManager.getActiveNetworkInfo() != null) {
+            if (connectivityManager.getActiveNetworkInfo().getType() == ConnectivityManager.TYPE_MOBILE
+                    && !prefs.getBoolean("mobilDataVarslet", false)) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.setMessage(R.string.mobilDataVarsel)
+                        .setPositiveButton(R.string.ok, null)
+                        .setNegativeButton("Ikke vis dette igjen", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                editor.putBoolean("mobilDataVarslet", true);
+                                editor.apply();
+                            }
+                        })
+                        .create()
+                        .show();
+            }
+        }
     }
 
     public void velgEgenskaper(View view) {
@@ -32,7 +56,6 @@ public class ChooseCategoryActivity extends AppCompatActivity {
         Intent nyIntent = new Intent(this.getBaseContext(), SetAttributesActivity.class);
 
         // Shared preferences
-        SharedPreferences.Editor editor = getSharedPreferences(MY_PREFS_NAME, MODE_PRIVATE).edit();
         editor.putString("Kategori", kategori);
         editor.apply();
 
