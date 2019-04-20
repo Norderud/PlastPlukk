dcdf9214e6fe0a1d85f21d0237d5521f811a91fb
Sannsynlighet <asmundno@live.no>
2019-04-20 16:44:31 +0200

    asd

--- app/src/main/java/no/usn/plastplukk/plastplukk/login/LoginRequest.java
+++ app/src/main/java/no/usn/plastplukk/plastplukk/login/LoginRequest.java
@@ -16,13 +16,8 @@ public class LoginRequest extends StringRequest {
     private static final String LOGIN_REQUEST_URL = String.format("%s%s", WEBURL, "login");
     private Map<String, String> params;
 
-    public LoginRequest(String user, String password, Response.Listener<String> listener){
-        super(Request.Method.POST, LOGIN_REQUEST_URL, listener, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                Log.e("error:", error.toString());
-            }
-        });
+    public LoginRequest(String user, String password, Response.Listener<String> listener, Response.ErrorListener errorListener){
+        super(Request.Method.POST, LOGIN_REQUEST_URL, listener, errorListener);
 
         params = new HashMap<>();
         params.put("user", user);
